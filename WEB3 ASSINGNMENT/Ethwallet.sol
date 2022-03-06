30b688f924956e518c0005c2e2eb27bbce131af0
Khadeeejah <wuraola81@gmail.com>
2022-03-06 22:05:54 +0100

    second

--- WEB3 ASSINGNMENT/Ethwallet.sol
+++ WEB3 ASSINGNMENT/Ethwallet.sol
@@ -1,24 +1,60 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.1;
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Wallet{
+
+    // receiver ether
+    // send ether
+    // store ether
+    // withdraw ether
+    // Get balance
+    // Get balance in contract
 
 
-contract Ethwallet {
-    address payable public owner;
+    // Events
+    event Deposit(address _addr, uint amount);
+    event Send(address _addr, uint amount);
+    event Withdraw(address _addr, uint amount);
 
-    constructor() {
-        owner = payable(msg.sender);
-
-
+    // Modifiers
+    modifier hasEnough(address _addr, uint _amount){
+        require(balances[_addr] >= _amount, "Insufficient funds");
+        _;
     }
 
-    receive () external payable {}
+    // Mapping
+    mapping(address => uint) public balances;
 
-    function withdraw (uint _amount) external{
-        require(msg.sender == owner, "caller is not ownwer");
-        payable(msg.sender).transfer(_amount);
+    // Receive Ether
+    receive() external payable{
+        balances[msg.sender] += msg.value;
+        emit Deposit(msg.sender, msg.value);
     }
 
-    function getBalance() external view returns (uint) {
+    // Send Ether
+    function send(address _receiver, uint _amount) external hasEnough(msg.sender, _amount){
+        balances[_receiver] += _amount;
+        balances[msg.sender] -= _amount;
+        emit Send(_receiver, _amount);
+    }
+
+    // Withdraw Ether
+    function withdraw(uint _amount) external hasEnough(msg.sender, _amount){
+        balances[msg.sender] -= _amount;
+        address payable _receiver = payable(msg.sender);
+        (bool sent, ) = _receiver.call{ value:_amount }("");
+        require(sent, "Ether not sent");
+        emit Withdraw(msg.sender, _amount);
+    }
+
+    // Get balance
+    function getBalance(address _addr) external view returns(uint){
+        return balances[_addr];
+    }
+
+    // Get balance in contract 
+    function getContractBalance() external view returns(uint){
         return address(this).balance;
     }
 }
