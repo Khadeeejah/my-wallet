75c5527585da32fc16352f9590b9e7b267418a08
Khadeeejah <wuraola81@gmail.com>
2022-03-07 23:38:29 +0100

    user

---
+++ SimpleStoragwUser.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+
+
+contract SimpleStorageUser{
+
+    // function add(string memory _name, uint256 _age, string memory _country) external override{
+
+    // }
+
+    function callAdd(address _addr,string memory _name, uint256 _age, string memory _country) external{
+    }
+}
